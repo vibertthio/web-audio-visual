2f49d2fb763b45580790b25d74c58cd70d3d8f92
Vibert Thio <vibertthio@gmail.com>
2018-01-14 16:06:29 +0800

    :speaker: add basic a/v scene

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+let xpos;
+let ypos;
+const damp = 0.1;
+
+// Microphone
+let mic;
+
 function setup() {
-  createCanvas(800, 500);
+  // createCanvas(800, 500);
+  createCanvas(windowWidth, windowHeight);
+  xpos = width * 0.5;
+  ypos = height * 0.5;
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  background(100);
-  stroke(0);
-  ellipse(100, 100, 50, 50);
+  background(40);
+  const vol = mic.getLevel();
+  const radius = map(vol, 0, 0.05, 100, 300);
+  noStroke();
+  fill(200, 190, 190);
+  ellipse(xpos, ypos, radius, radius);
+}
+
+function updatePos() {
+  xpos += (mouseX - xpos) * damp;
+  ypos += (mouseY - ypos) * damp;
 }
